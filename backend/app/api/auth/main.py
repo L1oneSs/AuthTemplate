18fb5610abdc73c3cde311ae24dd1854f012c9d8
L1ones <luhsm1974@gmail.com>
2025-05-12 15:19:53 +0300

    del: auth main.py

--- backend/app/api/auth/main.py
+++
@@ -1,9 +0,0 @@
-"""
-Основной модуль API аутентификации, объединяющий все эндпоинты
-"""
-from app.api.auth import api
-from app.api.auth.auth import Login, Logout, RefreshToken
-from app.api.auth.users import Register, UserProfile, UserSessions, UserSessionDetail, UserAvatar
-from app.api.auth.password import SetPassword, ResetPassword, SendPasswordEmail
-
-# Регистрация всех ресурсов API аутентификации
